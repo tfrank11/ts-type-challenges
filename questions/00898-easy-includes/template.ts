59865f3d705f0f22c67d2d03bd90684596ac3117
Timothy Frank <timmyfrank3@gmail.com>
2025-11-12 08:30:25 +1100

    WIP: 00898

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,7 @@
-type Includes<T extends readonly any[], U> = any
+type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest] ?
+  First extends U ?
+    true :
+    Rest extends [] ?
+      false :
+      Includes<Rest, U> :
+  false
