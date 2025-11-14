db1d1cbd67bfa7c79e17b3a0f8969384fd336f89
Timothy Frank <timmyfrank3@gmail.com>
2025-11-15 07:30:57 +1100

    00108

--- questions/00108-medium-trim/template.ts
+++ questions/00108-medium-trim/template.ts
@@ -1,1 +1,5 @@
-type Trim<S extends string> = any
+type Trim<S extends string> = S extends `${' ' | '\t' | '\n'}${infer Rest}`
+  ? Trim<Rest>
+  : S extends `${infer First}${' ' | '\t' | '\n'}`
+    ? Trim<First>
+    : S
