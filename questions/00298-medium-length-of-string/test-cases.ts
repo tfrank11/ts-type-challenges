eb76207ea11dfacd07943a273e3ffc12d8f021d1
Timothy Frank <timmyfrank3@gmail.com>
2025-11-16 15:42:30 +1100

    00298

--- questions/00298-medium-length-of-string/test-cases.ts
+++ questions/00298-medium-length-of-string/test-cases.ts
@@ -1,8 +1,8 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<LengthOfString<''>, 0>>,
-  Expect<Equal<LengthOfString<'kumiko'>, 6>>,
-  Expect<Equal<LengthOfString<'reina'>, 5>>,
-  Expect<Equal<LengthOfString<'Sound! Euphonium'>, 16>>,
+  Expect<Equal<LengthOfString00298<''>, 0>>,
+  Expect<Equal<LengthOfString00298<'kumiko'>, 6>>,
+  Expect<Equal<LengthOfString00298<'reina'>, 5>>,
+  Expect<Equal<LengthOfString00298<'Sound! Euphonium'>, 16>>,
 ]
