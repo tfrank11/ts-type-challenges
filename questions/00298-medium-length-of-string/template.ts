9b0d60ea4d936d630dd8cb19ef243f9434d68947
Timothy Frank <timmyfrank3@gmail.com>
2025-11-16 15:43:48 +1100

    00298 tweaks

--- questions/00298-medium-length-of-string/template.ts
+++ questions/00298-medium-length-of-string/template.ts
@@ -1,5 +1,3 @@
 type LengthOfString00298<S extends string, Acc extends unknown[] = []> = S extends `${infer _}${infer Rest}`
   ? LengthOfString00298<Rest, [...Acc, null]>
-  : Acc extends { length: infer L }
-    ? L
-    : never
+  : Acc['length']
