428999560120ad0fa6382c70b14c3856c890c055
Timothy Frank <timmyfrank3@gmail.com>
2025-11-21 09:22:33 +1100

    00949

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,3 @@
-type AnyOf<T extends readonly any[]> = any
+type AnyOf<T extends readonly any[]> = T[number] extends (0 | false | '' | { [k: string]: never } | [] | undefined | null)
+  ? false
+  : true
